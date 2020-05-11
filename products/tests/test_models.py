77fc2ce24a345023441b6ef3cd2eaca69f0b66ec
Rejaud Benjamin <benjamin.rejaud@gmail.com>
2020-05-11 20:22:38 +0200

    Add Test UniTest

---
+++ products/tests/test_models.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+
+class ProductTestsModels(TestCase):
+
+    def models_catedory(self):
+        pass
+
+    def models_store(self):
+        pass
+
+    def models_product(self):
+        pass
+
+    def models_favorite(self):
+        pass
---
+++ products/tests/test_views.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+class ProductTestViews(TestCase):
+
+    def views_result_search(self):
+        pass
+
+    def views_product_sheet(self):
+        pass
+
+    def views_favorites(self):
+        pass
+
