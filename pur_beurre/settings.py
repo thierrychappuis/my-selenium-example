ea8ca50d3433f29c0ae84467caada0132e111bc4
Rejaud Benjamin <benjamin.rejaud@gmail.com>
2020-05-21 19:54:49 +0200

    Modif settings

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".herokuapps.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["161.35.123.169", "indiana-p10.pythonclassmates.org", "localhost", "127.0.0.1"]
 
 
 # Application definition
