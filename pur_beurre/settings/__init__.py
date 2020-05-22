64782a925e653c4df44fea5d12b5b603a1a3c14a
Rejaud Benjamin <benjamin.rejaud@gmail.com>
2020-05-22 19:45:29 +0200

    modif settings

--- pur_beurre/settings/__init__.py
+++ pur_beurre/settings/__init__.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', "6AG2ldmCysksVrZnCKQmcz8Ovepr2sh4ALnRLU9N8yDhyiysPwBFNIPNy2AZTyL0")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
