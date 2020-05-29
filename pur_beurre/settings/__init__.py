6ed664da5b368f43a75ad274187078e826b022ea
Thierry Chappuis <thierry@chappuis.io>
2020-05-29 11:01:50 +0200

    Modified headless config for chrome

--- pur_beurre/settings/__init__.py
+++ pur_beurre/settings/__init__.py
@@ -13,16 +13,20 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', "6AG2ldmCysksVrZnCKQmcz8Ovepr2sh4ALnRLU9N8yDhyiysPwBFNIPNy2AZTyL0")
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    "6AG2ldmCysksVrZnCKQmcz8Ovepr2sh4ALnRLU9N8yDhyiysPwBFNIPNy2AZTyL0",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,14 +83,7 @@ WSGI_APPLICATION = 'pur_beurre.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pur_beurre',
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+    'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': 'db.sqlite3'}
 }
 
 
